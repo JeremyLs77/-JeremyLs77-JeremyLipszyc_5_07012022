98615cd0979b14d7323b7a5df18a9fa19a28d99f
Jeremy Ls <jldev8315@gmail.com>
2022-02-09 16:53:02 +0100

    Ajout fonction total des quantités

--- front/js/cart.js
+++ front/js/cart.js
@@ -68,6 +68,23 @@ function displayCart() {
 }
   }
 
+  function getTotal() {
+    // Total des quantités
+    var articleQuantity = document.getElementsByClassName("itemQuantity");
+    var articleLength = articleQuantity.length,
+      totalQuantity = 0;
+  
+    for (var i = 0; i < articleLength; ++i) {
+      totalQuantity += articleQuantity[i].valueAsNumber;
+    }
+  
+    let productTotalQuantity = document.getElementById("totalQuantity");
+    productTotalQuantity.innerHTML = totalQuantity;
+    console.log(totalQuantity);
+  }
+  getTotal();
+  
+
 // Supression d'articles dans le panier
 function deleteCart() {
   let delBtn = document.querySelectorAll('.deleteItem');
@@ -109,7 +126,7 @@ function modifyQtt() {
 
           localStorage.setItem("storedCart", JSON.stringify(openedCart));
       
-          // refresh rapide
+          // Refresh rapide
           location.reload();
 
           // Alerte de la mise à jour du panier
@@ -120,3 +137,92 @@ function modifyQtt() {
 modifyQtt();
 
 
+//-------- PARTIE FORMULAIRE
+
+
+/*Nom et Prénom */
+const form = document.getElementsByClassName("cart__order__form")[0];
+
+form.firstName.addEventListener("change", function () {
+  validName(this);
+});
+form.lastName.addEventListener("change", function () {
+  validName(this);
+});
+
+const validName = function (inputName) {
+  let nameRegExp = new RegExp("^[^- ][a-zA-Z '-àâäéèêëïîôöùûü]*[^- ]$", "g");
+  let testName = nameRegExp.test(inputName.value);
+  if (testName) {
+    inputName.nextElementSibling.innerHTML = "Validé";
+    inputName.nextElementSibling.style.color = "green";
+    return true;
+  } else {
+    inputName.nextElementSibling.innerHTML = "Saisissez votre prénom ou votre nom";
+    inputName.nextElementSibling.style.color = "red";
+    return false;
+  }
+};
+
+/*Adresse */
+form.address.addEventListener("change", function () {
+  validAddress(this);
+});
+
+const validAddress = function (inputAdress) {
+  let addressRegExp = new RegExp("^[0-9]{1,4} [^- ][a-zA-Z '-àâäéèêëïîôöùûü]*[^- ]$", "g");
+  let testAdress = addressRegExp.test(inputAdress.value);
+  if (testAdress) {
+    inputAdress.nextElementSibling.innerHTML = "Validé";
+    inputAdress.nextElementSibling.style.color = "green";
+    return true;
+  } else {
+    inputAdress.nextElementSibling.innerHTML = "Saisissez votre adresse";
+    inputAdress.nextElementSibling.style.color = "red";
+    return false;
+  }
+};
+
+/*Ville*/
+form.city.addEventListener("change", function () {
+  validCity(this);
+});
+
+const validCity = function (inputCity) {
+  let cityRegExp = new RegExp("^[^- ][a-zA-Z '-àâäéèêëïîôöùûü]*[^- ]$", "g");
+  let testCity = cityRegExp.test(inputCity.value);
+  if (testCity) {
+    inputCity.nextElementSibling.innerHTML = "Validé";
+    inputCity.nextElementSibling.style.color = "green";
+    return true;
+  } else {
+    inputCity.nextElementSibling.innerHTML = "Saisissez votre ville";
+    inputCity.nextElementSibling.style.color = "red";
+    return false;
+  }
+};
+
+/*Email*/
+form.email.addEventListener("change", function () {
+  validEmail(this);
+});
+
+const validEmail = function (inputEmail) {
+  let emailRegExp = new RegExp("^[a-zA-Z0-9.-_]+[@]{1}[a-zA-Z0-9.-_]+[.]{1}[a-z]{2,10}$","g");
+  let testEmail = emailRegExp.test(inputEmail.value);
+  if (testEmail) {
+    inputEmail.nextElementSibling.innerHTML = "Validé";
+    inputEmail.nextElementSibling.style.color = "green";
+    return true;
+  } else {
+    inputEmail.nextElementSibling.innerHTML ="Saisissez votre adresse mail complète";
+    inputEmail.nextElementSibling.style.color = "red";
+    return false;
+  }
+};
+
+//Envoi du formulaire
+//const sendOrderToBackEnd = (theOrder) => {
+//fetch("http://localhost:3000/api/products/order", {
+//method: "POST",
+// body: JSON.stringify(theOrder),
